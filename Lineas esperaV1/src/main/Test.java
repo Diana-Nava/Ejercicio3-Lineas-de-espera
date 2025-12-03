4ae03100fd74d2da7049fee427a7dce03041daf9
Diana-Nava <dianagnvhdz@gmail.com>
2025-12-02 20:37:04 -0600

    Inicio del código :D prueba 2

--- Lineas esperaV1/src/main/Test.java
+++ Lineas esperaV1/src/main/Test.java
@@ -16,6 +16,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Prueba uno");
         System.out.println("Prueba dos");
+        System.out.println("Prueba tres");
     }
     
 }
