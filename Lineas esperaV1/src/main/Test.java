84eddc2955e2d53a7dff11e47c5efdd612b0eee5
Diana-Nava <dianagnvhdz@gmail.com>
2025-12-03 16:12:11 -0600

    Intentando que finalmente se vea en el Git

--- Lineas esperaV1/src/main/Test.java
+++ Lineas esperaV1/src/main/Test.java
@@ -4,6 +4,8 @@
  */
 package main;
 
+import java.util.Scanner;
+
 /**
  *
  * @author diana
@@ -14,8 +16,30 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Prueba uno");
-        System.out.println("Papu");
+        Scanner reader=new Scanner(System.in);
+        System.out.println("Menu");
+        System.out.println("1. Modelo M/M/!");
+        System.out.println("2. Modelo M/M/C");
+        System.out.println("4. Modelo M/M/1K");
+        System.out.println("Opcion: ");
+        byte option=reader.nextByte();
+        
+        switch (option) {
+            case 1 -> {
+                System.out.println("Ingrese el valor de miu (U): ");
+                int U=reader.nextInt();
+                System.out.println("Ingrese el valor de lamda: ");
+                int lamda=reader.nextInt();
+                
+                System.out.println("Resultados: ");
+                double p= lamda / U;
+                System.out.println("La utilizacion del sistema es del " +(p*100)+ "%");
+                double Lq= Math.pow(lamda, 2)/U*(U-lamda);
+                System.out.println("El promedio de cliente en la cola es de: " + Lq + " clientes");
+                
+                
+            }
+        } 
     }
     
 }
