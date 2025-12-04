a37707b443fe4d1d2b7065ba472dc833f54724e7
Diana-Nava <dianagnvhdz@gmail.com>
2025-12-03 18:54:55 -0600

    Correciones del tipo de dato para el case 1, agregado de una opción para poder salir del menú y finalizar el código

--- Lineas esperaV1/src/main/Test.java
+++ Lineas esperaV1/src/main/Test.java
@@ -17,30 +17,39 @@ public class Test {
      */
     public static void main(String[] args) {
         Scanner reader   = new Scanner(System.in);
+        byte option = 0;
+        do {
         System.out.println("Menu");
         System.out.println("1. Modelo M/M/1");
         System.out.println("2. Modelo M/M/C");
-        System.out.println("4. Modelo M/M/1K");
+        System.out.println("3. Modelo M/M/1K");
+        System.out.println("4. Salir");
         System.out.println("Opcion: ");
-        byte option = reader.nextByte();
-
+        option = reader.nextByte();
+        
         switch (option) {
             case 1 -> {
                 System.out.println("Ingrese el valor de miu (U): ");
-                int U   = reader.nextInt();
+                double U = reader.nextInt();
                 System.out.println("Ingrese el valor de lamda: ");
-                int lamda   = reader.nextInt();
+                double lamda = reader.nextInt();
                 
                 System.out.println("Resultados: ");
                 double p   = lamda / U;
-                System.out.println("La utilizacion del sistema es del " +  ( p  * 100) + "%");
-                double Lq   = Math.pow(lamda,2)/U*(U-lamda);
+                double pf = p*100;
+                System.out.println("La utilizacion del sistema es del " + pf + "%");
+                double Lq   = Math.pow(lamda,2)/(U*(U-lamda));
                 System.out.println("El promedio de cliente en la cola es de: " + Lq + " clientes");
                 double Wq = Lq/lamda;
                 System.out.println("El tiempo promedio de espera en la cola es de "+Wq * 60+" minutos");
                 double w = Wq + (1/U);
                 System.out.println("El tiempo total del sistema es de "+ w * 60 +" minutos");
-         }    
          }
+            case 2 -> {
+                
+            }
+         }
+        
+        } while (option !=4);
     }
 }
