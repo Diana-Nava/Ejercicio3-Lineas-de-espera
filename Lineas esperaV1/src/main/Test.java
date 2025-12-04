4e2de8154db5c83366651d0ace679018c2c11ad0
Diana-Nava <dianagnvhdz@gmail.com>
2025-12-03 18:24:14 -0600

    Correciones, faltaban liineas de codigo que se perdieron al realizar el commit

--- Lineas esperaV1/src/main/Test.java
+++ Lineas esperaV1/src/main/Test.java
@@ -16,25 +16,25 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
+        Scanner reader   = new Scanner(System.in);
         System.out.println("Menu");
         System.out.println("1. Modelo M/M/1");
         System.out.println("2. Modelo M/M/C");
         System.out.println("4. Modelo M/M/1K");
         System.out.println("Opcion: ");
-        byte option = reader.nextByte();
+        byte option   = reader.nextByte(
 
         switch (option) {
             case 1 -> {
                 System.out.println("Ingrese el valor de miu (U): ");
-                int U = reader.nextInt();
+                int U   = reader.nextInt();
                 System.out.println("Ingrese el valor de lamda: ");
-                int lamda = reader.nextInt();
+                int lamda   = reader.nextInt(
 
                 System.out.println("Resultados: ");
-                double p  = lamda / U;
-                System.out.println("La utilizacion del sistema es del " + (p * 100) + "%");
-                double Lq  = Math.pow(lamda, 2 ) / U *( U -lamda);
+                double p   = lamda / U;
+                System.out.println("La utilizacion del sistema es del " +  ( p  * 1 00) + "%");
+                double Lq   = Math.pow(lamda,  2   )  /   U *( U -lamda);
                 System.out.println("El promedio de cliente en la cola es de: " + Lq + " clientes");
 
                 doule w = Wq + (1/U);
